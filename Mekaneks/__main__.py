cc330801f42998b5485dc764e3336700c3c514ee
Benjamin Baesu <bbaesu@gmail.com>
2020-02-18 15:32:02 -0800

    Update Game-Loop

--- Mekaneks/__main__.py
+++ Mekaneks/__main__.py
@@ -1,3 +1,30 @@
 import pygame
 import grid
 
+# initialize game engine
+pygame.init()
+
+window_width = 600
+window_height = 600
+
+animation_increment = 10
+clock_tick_rate = 20
+
+# Open a window
+display_size = (window_width, window_height)
+screen = pygame.display.set_mode(display_size)
+
+# Set title to the window
+pygame.display.set_caption('Mekaneks')
+
+gameExit = False
+
+
+while gameExit == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+    screen.blit(grid.grid(), [0, 0])
+
+    pygame.display.flip()
---
+++ Mekaneks/grid.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+def grid():
+    grid_image: object = pygame.image.load('/Users/Benny/Desktop/School/Software Engineering/mechanum/Mekaneks/gridBackground.png').convert()
+    return grid_image
+
+
+
+
