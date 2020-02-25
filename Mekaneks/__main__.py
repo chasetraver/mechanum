d722c36597bebec3912c9dbf03ab31cc414772fd
andrewchang7 <apchang@uci.edu>
2020-02-24 19:23:11 -0800

    main menu and button functionality

--- Mekaneks/__main__.py
+++ Mekaneks/__main__.py
@@ -1,27 +1,98 @@
 import pygame
 import grid
+import sys
 
-# initialize game engine
+# initialize game engine and open a window
+mainClock = pygame.time.Clock()
 pygame.init()
-
 window_width = 600
 window_height = 600
-
-# Open a window
+# window settings
 display_size = (window_width, window_height)
 screen = pygame.display.set_mode(display_size)
-
-# Set title to the window
 pygame.display.set_caption('Mekaneks')
 
-gameExit = False
+#main font (currently system default)
+font = pygame.font.SysFont(None, 20)
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
+click = False
 
-    # Print the grid to the screen
-    screen.blit(grid.grid(), [0, 0])
+def main_menu():
+    while True:
+        #button texts
+        text_play = font.render('Mechaneks', True, (255,255,255))
+        #screen.blit(text_play, (0,0))
+        #Fill black
+        screen.fill((0,0,0))
+        #button creations
+        button_play = pygame.Rect(200, 200, 200, 50)
+        button_options = pygame.Rect(200, 300, 200, 50)
+        button_exit = pygame.Rect(200, 400, 200, 50)
+        pygame.draw.rect(screen, (255,0,0), button_play)
+        pygame.draw.rect(screen, (255,0,0), button_options)
+        pygame.draw.rect(screen, (255,0,0), button_exit)
+        pygame.draw.rect(screen, (255,0,0), button_play)
 
-    pygame.display.flip()
+        for event in pygame.event.get():
+            mx, my = pygame.mouse.get_pos()
+            click = False
+            if event.type == pygame.QUIT:
+                exit()
+            #event - left mousebutton clicked (button actions)
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                click = True
+                if button_play.collidepoint(mx,my):
+                    if click:
+                        game()
+                if button_options.collidepoint(mx,my):
+                    if click:
+                        options()
+                if button_exit.collidepoint(mx,my):
+                    if click:
+                        exit()
+            #call exit function on Esc key
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+        pygame.display.update()
+        mainClock.tick(60)
+
+def game():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+        #Print the grid to the screen
+        screen.blit(grid.grid(), [0, 0])
+        pygame.display.flip()
+        pygame.display.update()
+        mainClock.tick(60)
+
+def options():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+        #Print the grid to the screen
+        screen.blit(grid.grid(), [0, 0])
+        pygame.display.flip()
+        pygame.display.update()
+        mainClock.tick(60)
+
+def exit():
+    pygame.quit()
+    sys.exit()
+
+main_menu()
--- Mekaneks/grid.py
+++ Mekaneks/grid.py
@@ -2,7 +2,7 @@ import pygame
 
 
 def grid():
-    grid_image: object = pygame.image.load('/Users/Benny/Desktop/School/Software Engineering/mechanum/Mekaneks/gridBackground.png').convert()
+    grid_image: object = pygame.image.load('gridBackground.png').convert()
     return grid_image
 
 
