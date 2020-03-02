208660ca5ff9bd54ac6fbeea5fefbf93e1a137fb
chasetraver <chasetraver@yahoo.com>
2020-03-01 22:44:55 -0800

    goblin monster image

--- Mekaneks/monster.py
+++ Mekaneks/monster.py
@@ -5,6 +5,7 @@ class Monster:
     def __init__(self, hp):
         self.hp = hp
         self.isalive = 1
+        self.sprite = pygame.image.load()
 
     def damage(self, amount):
         self.hp = self.hp - amount
