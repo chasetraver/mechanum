cb75b7b49110a143c52b500238be4ab0c26132c4
chasetraver <60958060+chasetraver@users.noreply.github.com>
2020-03-07 10:34:01 -0800

    added file to store current and future cards, as well as a function to return a random card from all cards.

---
+++ Mekaneks/cardlib.py
@@ -0,0 +1,20 @@
+import deckcardplayerclasses
+import random
+
+pointedstick = deckcardplayerclasses.Card("Pointed Stick", 0, 1, 1, 0, 'pointedstick.png')
+conveyorwheels = deckcardplayerclasses.Card("Conveyor Wheels", 1, 0, 0, 0, 'conveyorwheels.png')
+scraparmor = deckcardplayerclasses.Card("Scrap Armor", 0, 0, 0, 1, 'scraparmor.png')
+sticklobber = deckcardplayerclasses.Card("Stick Lobber", 0, 2, 1, 0, 'sticklobber.png')
+
+allcards = [pointedstick, conveyorwheels, scraparmor, sticklobber]
+
+
+def startingcards():
+    startcards = [pointedstick, conveyorwheels, scraparmor, sticklobber]
+    return startcards
+
+
+def randomcard():
+    i = (len(allcards))
+    r = random.randint(0, i)
+    return allcards[r]
